3103ea265393969f20ad7992a2470cb6d20d2c84
ErdbeerbaerLP <27149563+ErdbeerbaerLP@users.noreply.github.com>
2019-05-05 20:24:01 +0200

    Change version number

--- src/main/java/de/erdbeerbaerlp/guilib/McMod.java
+++ src/main/java/de/erdbeerbaerlp/guilib/McMod.java
@@ -11,7 +11,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class McMod {
 	public static final String MODID = "eguilib";
 	public static final String NAME = "Erdbeerbaer´s GuiLib";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "0.1.0";
 	public McMod() {
 		MinecraftForge.EVENT_BUS.register(this);
 	}
