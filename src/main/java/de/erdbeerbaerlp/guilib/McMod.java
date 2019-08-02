95f435cde48461ef1e5b97c6154287f06e896b20
ErdbeerbaerLP <27149563+ErdbeerbaerLP@users.noreply.github.com>
2019-08-02 18:47:42 +0200

    Update version numbers

--- src/main/java/de/erdbeerbaerlp/guilib/McMod.java
+++ src/main/java/de/erdbeerbaerlp/guilib/McMod.java
@@ -6,5 +6,5 @@ import net.minecraftforge.fml.common.Mod;
 public class McMod {
     public static final String MODID = "eguilib";
     public static final String NAME = "Erdbeerbaer´s GuiLib";
-    public static final String VERSION = "0.1.0";
+    public static final String VERSION = "0.1.1";
 }
