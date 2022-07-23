10b8b72f0745ea783da4e875875e2acba346c364
ErdbeerbaerLP <admin@erdbeerbaerlp.de>
2022-07-23 16:57:06 +0200

    Fix for newer forge version

--- src/main/java/de/erdbeerbaerlp/guilib/McMod.java
+++ src/main/java/de/erdbeerbaerlp/guilib/McMod.java
@@ -1,6 +1,7 @@
 package de.erdbeerbaerlp.guilib;
 
-import net.minecraftforge.client.ConfigGuiHandler;
+
+import net.minecraftforge.client.ConfigScreenHandler;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
@@ -10,12 +11,12 @@ import org.apache.logging.log4j.Logger;
 public class McMod {
     public static final String MODID = "eguilib";
     public static final String NAME = "Erdbeerbaer´s GuiLib";
-    public static final String VERSION = "2.0.0";
+    public static final String VERSION = "2.0.3";
     public static final Logger LOGGER = LogManager.getLogger();
 
     public McMod() {
-        ModLoadingContext.get().registerExtensionPoint(ConfigGuiHandler.ConfigGuiFactory.class,
-                () -> new ConfigGuiHandler.ConfigGuiFactory((mc, screen) -> new ExampleGUI(screen)));
+        ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class,
+                () -> new ConfigScreenHandler.ConfigScreenFactory((mc, screen) -> new ExampleGUI(screen)));
 
     }
 
